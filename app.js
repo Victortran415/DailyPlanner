11bc90a4bba2076be2dd5880fb98e2baae5a4a99
Victor Tran <victortran415@gmail.com>
2020-10-30 17:04:07 -0700

    added color to textarea

--- app.js
+++ app.js
@@ -1,47 +1,35 @@
 $(document).ready(function() {
-    const currentTime = moment().format('MMMM Do YYYY, H:mm:ss a');
+    const currentTime = moment().format('MMMM Do YYYY, H:mm a');
     $("#currentDay").text(currentTime)
-
-    console.log(currentTime)
-
-    
-
-
-
-
     const times = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
     times.forEach(time => {
         const timeCheck = window.localStorage.getItem(time);
-
-
         const currentHour = moment().hour()
-
-        console.log(currentHour)
-        console.log(time)
+        const timeVal = "#" + time
 
         if (currentHour > time) {
-            $(`#${time}`).addClass("bg-danger text-whitesmoke")
-            $(`#${time}`).attr("disabled", true)
-        } else if (currentTime === time) {
-            $(`#${time}`).addClass("bg-secondary text-whitesmoke")
+            $(timeVal).addClass("bg-danger")
+            $(timeVal).attr("disabled", true)
+        } else if (currentHour === time) {
+            $(timeVal).addClass("bg-success text-light")
         } else {
-            $(`#${time}`).addClass("bg-success text-whitesmoke")
+            $(timeVal).addClass("bg-secondary text-light")
         }
 
         if (timeCheck === null) {
             window.localStorage.setItem(time, "")
         } else if (timeCheck.length > 0 ) {
-            $(`#${time}`).attr("value", window.localStorage.getItem(time))
+            $(timeVal).attr("value", window.localStorage.getItem(time)) //NOTE: text not showing on textarea, need to rework
         }
     })
 
     $("form").on("submit", function (e) {
         e.preventDefault();
         const time = (e.target.querySelector("textarea").getAttribute("id"))
-        console.log(time) NOTE: //seeing if the time will appear to adding text
+        console.log(time) //NOTE: //seeing if the time will appear to adding text
         const text =(e.target.querySelector("textarea").value)
-        console.log(text) NOTE: //seeing if the text will appear to adding text
+        console.log(text) //NOTE: //seeing if the text will appear to adding text
         window.localStorage.setItem(time, text)
     })
     
